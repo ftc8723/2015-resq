ca621fdee5f16e7988ece7c29505ac914064d57e
Tim Dawson <tim.dawson@gmail.com>
2015-11-23 23:46:37 -0600

    adding STOP step, improving debugging options

--- FtcRobotController/src/main/java/org/usfirst/ftc8723/FredAuto.java
+++ FtcRobotController/src/main/java/org/usfirst/ftc8723/FredAuto.java
@@ -2,8 +2,10 @@ package org.usfirst.ftc8723;
 
 public class FredAuto extends FredHardware {
 
-	// used in the loop() method to track which step of autonomous we are in
-	int step = 0;
+	// used in the loop() method to track which step of autonomous we are in and how long we have run
+	private int step = 0;
+	private long start = 0;
+	private long runtime = 0;
 
 	/*
      * Code to run when the op mode is first enabled goes here
@@ -35,35 +37,43 @@ public class FredAuto extends FredHardware {
 			// Synchronize the state machine and hardware.
 			//
 			case 0:
+				start = System.currentTimeMillis();
 				resetDriveEncoders();
 				step++;
 				break;
 			case 1:
 				if (haveDriveEncodersReset()) {
 					runUsingEncoders();
-					setDrivePower(1.0f, 1.0f);
+					setDrivePower(0.5f, 0.5f);
 					step++;
 				}
 				break;
 			case 2:
-				if (haveDriveEncodersReached(2880, 2880)) { // todo change values
+				if (haveDriveEncodersReached(1000, 1000)) { // todo change values
 					resetDriveEncoders();
+					setArmPosition(0.1);
+					setBucketPosition(0.5);
 					setDrivePower(0.0f, 0.0f);
 					step++;
 				}
 				break;
 			case 3:
-				if (haveDriveEncodersReset()) {
+				if (haveDriveEncodersReset() && timeSinceStart() > 2000) {
 					runUsingEncoders();
 					setDrivePower(-0.5f, 0.5f);
+					setArmPosition(0.5);
+					setBucketPosition(0.1);
 					step++;
 				}
 				break;
 			case 4:
-				if (haveDriveEncodersReached(2880, 2880)) { // todo change values
+				if (haveDriveEncodersReached(1000, 1000)) { // todo change values
 					resetDriveEncoders();
 					setDrivePower(0.0f, 0.0f);
+					setArmPosition(0.2);
+					setBucketPosition(0.2);
 					step++;
+					step = STOP; // todo move this further down as we get things working
 				}
 				break;
 			case 5:
@@ -190,15 +200,28 @@ public class FredAuto extends FredHardware {
 				break;
 			default:
 				// final state - autonomous actions have been accomplished
+				step = STOP;
+				if (runtime == 0) runtime = timeSinceStart();
 				break;
 		}
-
-		telemetry.addData("Step", step);
 	}
 
 	@Override
 	protected void updateTelemetry() {
 		super.updateTelemetry();
-		telemetry.addData("Autonomous Step ", step);
+		if (step == STOP) {
+			telemetry.addData("Autonomous Complete ", String.format("%.2f sec", ((double)runtime)/1000));
+		} else {
+			telemetry.addData("Autonomous Step ", step);
+			telemetry.addData("Autonomous Time ", String.format("%.2f", ((double)timeSinceStart())/1000));
+		}
 	}
+
+	// return the current time minus the start time
+	private long timeSinceStart() {
+		return System.currentTimeMillis() - start;
+	}
+
+	// constant used for end of program
+	static final int STOP = Integer.MAX_VALUE;
 }
